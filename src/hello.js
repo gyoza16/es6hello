970df271f51112a6a796896ba3ace59d71d1340c
gyoza16 <sxmxoxt@gmail.com>
2019-04-15 22:30:55 -0700

    Add files to run a class based very simple example

---
+++ src/hello.js
@@ -0,0 +1,14 @@
+export default class Hello {
+  consructor() {
+  }
+
+  /**
+   * 挨拶をする
+   * @returns {string}
+   */
+  sayHello() {
+    const hello = 'Hi, there!';
+    console.log(hello);
+    return hello;
+  }
+}
