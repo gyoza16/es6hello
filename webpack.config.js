ffdaf986c6f6304ac6465414206fec4f072f8f27
gyoza16 <sxmxoxt@gmail.com>
2019-04-17 12:49:22 -0700

    Add babel support

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   devServer: {
+    path: path.join(__dirname, "dist"),
     open: true,
     openPage: 'index.html',
     contentBase: path.join(__dirname, 'public'),
@@ -15,5 +16,28 @@ module.exports = {
     filename: '[name].js',
     library: ['com', 'example'],
     libraryTarget: 'umd'
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                "useBuiltIns": "usage",
+                "targets": "> 0.25%, not dead"
+                }
+              ]
+            ]
+          }
+        }
+      }
+    ]
+  },
+  devtool: 'inline-source-map'
 };
